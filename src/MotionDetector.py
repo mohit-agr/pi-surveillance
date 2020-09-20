cb873027c7daeaa1ac95ceb7ef41e15904415bec
webmohit <mohit.agarwal@tum.de>
2020-09-20 13:52:55 +0200

    added Flask App for live stream

--- src/MotionDetector.py
+++ src/MotionDetector.py
@@ -3,8 +3,11 @@ import time
 import datetime
 import threading
 import numpy as np
+import flask
 
 timerSetter = 10
+app = flask.Flask(__name__)
+video_getter = VideoGetter(0)
 
 class VideoGetter:
     def __init__(self, src=0):
@@ -39,7 +42,6 @@ def timer(motion_detected_event):
 
 def detect_motion():
     global timerSetter
-    video_getter = VideoGetter(0).start()
     frame1 = video_getter.frame
     gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     motion_detected_event = threading.Event()
@@ -56,15 +58,36 @@ def detect_motion():
         if np.any(threshold == 255):
             motion_detected_event.set()
             timerSetter = 0
-        # if motion_detected_event.is_set():
-        #     # TODO: send notification and start streaming
-        #     cv2.imshow('window', frame2)
-        # if cv2.waitKey(1) == ord('q'):
-        #     cv2.destroyAllWindows()
-        #     thread_timer.join(1)
-        #     break
+        if motion_detected_event.is_set():
+            # TODO: send notification and start streaming
+            cv2.imshow('window', frame2)
+        if cv2.waitKey(1) == ord('q'):
+            cv2.destroyAllWindows()
+            thread_timer.join(1)
+            break
 
         gray1 = gray2
 
+@app.route('/')
+def index():
+   """Video streaming ."""
+   return flask.render_template('index.html')
+
+def gen():
+   """Video streaming generator function."""
+   while True:
+       frame = video_getter.frame
+       cv2.imwrite('pic.jpg', frame)
+       yield (b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+   """Video streaming route. Put this in the src attribute of an img tag."""
+   return flask.Response(gen(),
+                   mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == '__main__':
+    video_getter.start()
+    app.run(host='0.0.0.0', debug=True, threaded=True, )
     detect_motion()
