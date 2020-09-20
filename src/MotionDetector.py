fa19f1e7365d2d01b5e5295755abce9f592d7a0a
webmohit <mohit.agarwal@tum.de>
2020-09-20 12:50:21 +0200

    multiprocessing is not needed for now, switch to threading

--- src/MotionDetector.py
+++ src/MotionDetector.py
@@ -1,46 +1,70 @@
 import cv2
 import time
 import datetime
-from multiprocessing import Process, Event, Value
+import threading
 import numpy as np
 
-def Timer(timerSetter, motionDetectedEvent):
-    while True:
-        motionDetectedEvent.wait()
-        # print (datetime.datetime.now().time(), "started new timer")
-        while timerSetter.value < 10:
-            time.sleep(1)
-            timerSetter.value += 1
-        motionDetectedEvent.clear()
+timerSetter = 10
 
-def DetectMotion(cap):
-    cap.read()
-    cap.read()
-    ret1,frame1= cap.read()
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    timerSetter = Value('i', 10)
-    motionDetectedEvent = Event()
-    timerProcess = Process(name='timer', target=Timer, args=(timerSetter,motionDetectedEvent,))
-    timerProcess.start()
+class VideoGetter:
+    def __init__(self, src=0):
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        threading.Thread(target=self.get, args=()).start()
+        return self
+
+    def get(self):
+        while not self.stopped:
+            if not self.grabbed:
+                self.stop()
+            else:
+                (self.grabbed, self.frame) = self.stream.read()
+
+    def stop(self):
+        self.stopped = true
+
+def timer(motion_detected_event):
+    global timerSetter
     while True:
-        if timerSetter.value >= 10:
-            cv2.destroyAllWindows()
+        motion_detected_event.wait()
+        # print (datetime.datetime.now().time(), "started new timer, thread Id", threading.get_ident())
+        while timerSetter < 10:
             time.sleep(1)
-        ret2,frame2=cap.read()
+            timerSetter += 1
+
+        motion_detected_event.clear()
+
+def detect_motion():
+    global timerSetter
+    video_getter = VideoGetter(0).start()
+    frame1 = video_getter.frame
+    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    motion_detected_event = threading.Event()
+    thread_timer = threading.Thread(name='timer', target=timer, args=(motion_detected_event,))
+    thread_timer.start()
+    while True:
+        if not motion_detected_event.is_set():
+            time.sleep(1)
+        frame2 = video_getter.frame
         gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
         deltaframe = cv2.absdiff(gray1, gray2)
         threshold = cv2.threshold(deltaframe, 100, 255, cv2.THRESH_BINARY)[1]
         if np.any(threshold == 255):
-            motionDetectedEvent.set()
-            timerSetter.value = 0
-        if motionDetectedEvent.is_set():
-            # TODO: send notification and start streaming
+            motion_detected_event.set()
+            timerSetter = 0
+        # if motion_detected_event.is_set():
+        #     # TODO: send notification and start streaming
         #     cv2.imshow('window', frame2)
-        #     cv2.waitKey(20)
+        # if cv2.waitKey(1) == ord('q'):
+        #     cv2.destroyAllWindows()
+        #     thread_timer.join(1)
+        #     break
 
         gray1 = gray2
 
 if __name__ == '__main__':
-    cap = cv2.VideoCapture(0)
-    DetectMotion(cap)
+    detect_motion()
