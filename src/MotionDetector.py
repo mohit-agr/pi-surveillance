f97c8e3cc7cbdc08895abfba9e0f111671adef15
webmohit <mohit.agarwal@tum.de>
2020-09-21 22:11:44 +0200

    uploading to azure blob storage

--- src/MotionDetector.py
+++ src/MotionDetector.py
@@ -3,15 +3,16 @@ import time
 import datetime
 import threading
 import numpy as np
-import flask
+import multiprocessing
+from src.UploadToStorage import ImageUploader
 
 timerSetter = 10
-app = flask.Flask(__name__)
-video_getter = VideoGetter(0)
 
 class VideoGetter:
-    def __init__(self, src=0):
+    def __init__(self, src, motion_detected_event, img_queue):
         self.stream = cv2.VideoCapture(src)
+        self.motion_detected_event = motion_detected_event
+        self.image_queue = img_queue
         (self.grabbed, self.frame) = self.stream.read()
         self.stopped = False
 
@@ -24,10 +25,12 @@ class VideoGetter:
             if not self.grabbed:
                 self.stop()
             else:
+                if self.motion_detected_event.is_set():
+                    self.image_queue.put(self.frame)
                 (self.grabbed, self.frame) = self.stream.read()
 
     def stop(self):
-        self.stopped = true
+        self.stopped = True
 
 def timer(motion_detected_event):
     global timerSetter
@@ -42,52 +45,36 @@ def timer(motion_detected_event):
 
 def detect_motion():
     global timerSetter
+    motion_detected_event = threading.Event()
+    img_queue = multiprocessing.Queue()
+
+    video_getter = VideoGetter(0, motion_detected_event, img_queue).start()
+    threading.Thread(name='timer', target=timer, args=(motion_detected_event,)).start()
+    ImageUploader(img_queue).start()
+
     frame1 = video_getter.frame
     gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    motion_detected_event = threading.Event()
-    thread_timer = threading.Thread(name='timer', target=timer, args=(motion_detected_event,))
-    thread_timer.start()
+
     while True:
         if not motion_detected_event.is_set():
             time.sleep(1)
         frame2 = video_getter.frame
         gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-        deltaframe = cv2.absdiff(gray1, gray2)
-        threshold = cv2.threshold(deltaframe, 100, 255, cv2.THRESH_BINARY)[1]
+        delta_frame = cv2.absdiff(gray1, gray2)
+        threshold = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)[1]
         if np.any(threshold == 255):
             motion_detected_event.set()
             timerSetter = 0
-        if motion_detected_event.is_set():
-            # TODO: send notification and start streaming
-            cv2.imshow('window', frame2)
-        if cv2.waitKey(1) == ord('q'):
-            cv2.destroyAllWindows()
-            thread_timer.join(1)
-            break
+        # if motion_detected_event.is_set():
+        #     # TODO: send notification
+        #     cv2.imshow('window', frame2)
+        # if cv2.waitKey(1) == ord('q'):
+        #     cv2.destroyAllWindows()
+        #     thread_timer.join(1)
+        #     break
 
         gray1 = gray2
 
-@app.route('/')
-def index():
-   """Video streaming ."""
-   return flask.render_template('index.html')
-
-def gen():
-   """Video streaming generator function."""
-   while True:
-       frame = video_getter.frame
-       cv2.imwrite('pic.jpg', frame)
-       yield (b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-   """Video streaming route. Put this in the src attribute of an img tag."""
-   return flask.Response(gen(),
-                   mimetype='multipart/x-mixed-replace; boundary=frame')
-
 if __name__ == '__main__':
-    video_getter.start()
-    app.run(host='0.0.0.0', debug=True, threaded=True, )
     detect_motion()
---
+++ src/UploadToStorage.py
@@ -0,0 +1,29 @@
+from azure.storage.blob import BlobServiceClient
+import cv2
+import multiprocessing
+import datetime
+
+class ImageUploader:
+    def __init__(self, image_stream):
+        connection_string = open('secrets/BlobStorageConnectionString.txt', 'r').read()
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        container_name = 'pi-surveillance'
+
+        try:
+            self.blob_container_client = blob_service_client.create_container(name=container_name)
+        except Exception as e:
+            self.blob_service_client = blob_service_client.get_container_client(container=container_name)
+
+        self.image_stream = image_stream
+        self.process = multiprocessing.Process(target=self.upload, args=())
+
+    def start(self):
+        self.process.start()
+
+    def upload(self):
+        while True:
+            img = self.image_stream.get()
+            img_str = cv2.imencode('.jpg', img)[1].tobytes()
+            blob_name = datetime.datetime.utcnow().isoformat(sep=' ', timespec='milliseconds') + '.jpg'
+            print('created container client' + self.blob_container_client.container_name)
+            self.blob_container_client.upload_blob(name=blob_name, data=img_str)
